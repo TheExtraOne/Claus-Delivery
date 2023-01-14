0154f7b8e338e1887cfc61cd25e1c75dccecbe49
TheExtraOne <katya_mihasewa@mail.ru>
2023-01-14 22:18:33 +0300

    feat: add animation for cart button

--- src/components/layout/header-cart-button.js
+++ src/components/layout/header-cart-button.js
@@ -1,22 +1,42 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../cart/cart-icon";
 import classes from "./header-cart-button.module.css";
 import { clientEvents } from "../emiter/client-events";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const ctx = useContext(CartContext);
+
   const openCart = () => {
     clientEvents.emit("ECartClicked");
   };
-
-  const ctx = useContext(CartContext);
 
   const numberOfCartItems = ctx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (ctx.items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 800);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [ctx.items]);
+
   return (
-    <button className={classes.button} onClick={openCart}>
+    <button className={btnClasses} onClick={openCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
