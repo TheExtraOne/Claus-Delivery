2725267ea1f01904d6add7b114016e8607d32572
TheExtraOne <katya_mihasewa@mail.ru>
2023-01-14 18:45:55 +0300

    feat: add cart modal through portal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Header from './components/layout/header';
-import Meals from './components/meals/meals';
+import Header from "./components/layout/header";
+import Meals from "./components/meals/meals";
+import Cart from "./components/cart/cart";
 
 function App() {
   return (
     <>
+      <Cart />
       <Header />
       <main>
         <Meals />
